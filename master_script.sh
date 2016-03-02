5ffca8ae8256ebfe51b9a28ba21dca852541063e
Clément Renaud <clement.renaud@gmail.com>
2016-03-02 17:07:28 +0100

    a masterscript to rule them all ! mwouhahah

---
+++ master_script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# parse ARC5 file data
+
+echo
+echo "PARSE DATA FROM FILE "
+echo "#############"
+echo
+cd arc5-parser
+. venv/bin/activate
+# pip install -r requirements.txt
+python parse_partenaires.py
+python parse_subventions.py
+python parse_dashboard.py
+deactivate
+cd ..
+
+# parse ARCs DB and merge into single mongo collection
+echo
+echo "PARSE ARC DATABASE "
+echo "#############"
+echo
+cd wp-api-crawler
+node app.js
+cd ..
+
+# export mongo collection to CSV
+cd mongoexport
+sh export_to_csv.sh
+cd ..
+
+# clean the data (remove duplicate, etc)
+cd matrix
+. venv/bin/activate
+python matrix.py
+deactivate
+cd ..
